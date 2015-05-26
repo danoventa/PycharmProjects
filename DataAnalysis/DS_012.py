e3e14e33fb3fd30fc67263d5a645bdb1611c7e9b
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-26 00:01:15 -0500

    Regression continued:

--- DataAnalysis/DS_012.py
+++ DataAnalysis/DS_012.py
@@ -14,37 +14,50 @@ tips = sns.load_dataset('tips')
 print(tips.head())
 
 sns.lmplot('total_bill', 'tip', tips)
-plt.show()
+#plt.show()
 
 sns.lmplot('total_bill', 'tip', tips,
            scatter_kws={'marker':'o', 'color':'indianred'},
            line_kws={'linewidth':1, 'color':'blue'})
-plt.show()
+#plt.show()
 
 sns.lmplot('total_bill', 'tip', tips, order=4,
            scatter_kws={'marker':'o', 'color':'indianred'},
            line_kws={'linewidth':1, 'color':'blue'})
-plt.show()
+#plt.show()
 
 sns.lmplot('total_bill', 'tip', tips, fit_reg=False)
-plt.show()
+#plt.show()
 
 tips['tip_perc'] = 100*(tips['tip']/tips['total_bill'])
 print(tips.head())
 
 sns.lmplot('size', 'tip_perc', tips)
-plt.show()
+#plt.show()
 
 sns.lmplot('size', 'tip_perc', tips, x_jitter=0.1)
-plt.show()
+#plt.show()
 
 sns.lmplot('size', 'tip_perc', tips, x_estimator=np.mean)
-plt.show()
+#plt.show()
 
 sns.lmplot('total_bill', 'tip_perc', tips, hue='sex', markers=['x', 'o'])
-plt.show()
+#plt.show()
 
 sns.lmplot('total_bill', 'tip_perc', tips, hue='day')
+#plt.show()
+
+
+''' Does not work, something wrong with how the statsmodel is implemented in the API, or installed on my machine. .
+    or it just doesn't work with python 3.4... I've installed it a few times, in a few different ways, still no
+    dice! '''
+#sns.lmplot('total_bill', 'tip_perc', tips, lowess=True, line_kws={'color':'black'})
+#plt.show()
+
+sns.regplot('total_bill', 'tip_perc', tips)
 plt.show()
 
-
+fig, (axis1, axis2) = plt.subplots(1, 2, sharey=True)
+sns.regplot('total_bill', 'tip_perc', tips, ax=axis1)
+sns.violinplot(tips['tip_perc'], tips['size'], color='Reds_r', ax=axis2)
+plt.show()
