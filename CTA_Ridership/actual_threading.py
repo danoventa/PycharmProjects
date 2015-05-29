d9dfe2aaf70db54b4720a9e0b23292bc3c6c716e
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-29 15:40:09 -0500

    Threading done, no errors so far. Looks like it's running! Need to see how to control thread counts

--- CTA_Ridership/actual_threading.py
+++ CTA_Ridership/actual_threading.py
@@ -2,6 +2,8 @@ __author__ = 'Noventa'
 
 import pandas as pd
 import sqlite3 as db
+import threading as th
+import time
 
 from geopy.distance import vincenty
 from decimal import Decimal
@@ -28,6 +30,27 @@ visited = {}
 x_p = -1
 y_p = 0
 
+
+''' max value object to be used with the threading.'''
+class Max_Value(object):
+    def __init__(self, max=0.0):
+        self.lock = th.Lock()
+        self.value = max
+    def max_value(self, new_max):
+        self.lock.acquire()
+        try:
+            self.value = new_max
+        finally:
+            self.lock.release()
+
+'''calculate and use the thread safe object '''
+def calculate_max(px, py, max_obj):
+    distance = vincenty(px, py).miles.real
+    if distance > max_obj.value:
+        max_obj.max_value(new_max=distance)
+
+max = Max_Value()
+
 for row_x in test1[loc]:
     loc_x = extract_location(row_x)
     x_p += 1
@@ -36,7 +59,17 @@ for row_x in test1[loc]:
     for row_y in test1[loc]:
         if  y_p > x_p:
             loc_y = extract_location(row_y)
-            distance = vincenty(loc_x, loc_y).miles.real
-            if distance > max:
-                max = distance
+            t = Thread(target=calculate_max, args=(loc_x, loc_y, max))
+            t.start()
         y_p += 1
+
+''' don't particularly understand this purpose.'''
+main_thread = th.currentThread()
+start_time = time.time()
+for t in th.enumerate():
+    if t is not main_thread:
+        t.join()
+
+total_time =  time.time().real - start_time.real
+print("Total Time: " + total_time)
+print("Max Value: " + max.max_value)
