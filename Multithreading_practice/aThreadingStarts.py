c10f5599cd5d304958128b74e7ef67aaf8223d1c
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-29 13:39:10 -0500

    queuing with threading project started.

---
+++ Multithreading_practice/aThreadingStarts.py
@@ -0,0 +1,22 @@
+__author__ = 'Noventa'
+
+from queue import Queue
+from threading import Thread
+
+q = Queue(maxsize=0)
+num_threads = 10
+
+def do_stuff(q):
+  while True:
+    print(q.get())
+    q.task_done()
+
+for i in range(num_threads):
+  worker = Thread(target=do_stuff, args=(q,))
+  worker.setDaemon(True)
+  worker.start()
+
+for x in range(100):
+    q.put(x)
+
+q.join()
