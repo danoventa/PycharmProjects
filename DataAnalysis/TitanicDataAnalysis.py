e8728266f723c42c9b6fe2e03594fe7604c98638
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-31 21:11:50 -0500

    A few more plots

--- DataAnalysis/TitanicDataAnalysis.py
+++ DataAnalysis/TitanicDataAnalysis.py
@@ -1,8 +1,12 @@
 __author__ = 'Noventa'
 
+
+
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import bokeh as bo
@@ -26,14 +30,35 @@ def male_female_child(passenger):
 
 titanic_df['person'] = titanic_df[['Age', 'Sex']].apply(male_female_child, axis=1)
 print(titanic_df[0:10])
-
 sns.factorplot('Pclass', data=titanic_df, hue='person')
-
 print(titanic_df[0:10])
-
 plt.show()
-
 titanic_df['Age'].hist(bins=70)
 plt.show()
-
 print(titanic_df['Age'].mean())
+
+print(titanic_df['person'].value_counts())
+
+fig = sns.FacetGrid(titanic_df, hue='Sex', aspect=4)
+fig.map(sns.kdeplot, 'Age', shade=True)
+oldest = titanic_df['Age'].max()
+fig.set(xlim=(0, oldest))
+fig.add_legend()
+plt.show()
+
+
+fig = sns.FacetGrid(titanic_df, hue='person', aspect=4)
+fig.map(sns.kdeplot, 'Age', shade=True)
+oldest = titanic_df['Age'].max()
+fig.set(xlim=(0, oldest))
+fig.add_legend()
+
+plt.show()
+
+fig = sns.FacetGrid(titanic_df, hue='Pclass', aspect=4)
+fig.map(sns.kdeplot, 'Age', shade=True)
+oldest = titanic_df['Age'].max()
+fig.set(xlim=(0, oldest))
+fig.add_legend()
+
+plt.show()
