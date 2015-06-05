20362d336508688d073ef33a99f26141bfcc3a3a
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-06-04 22:30:48 -0500

    Recursive in tail and normal recursion.

--- Mathematical/Sum.py
+++ Mathematical/Sum.py
@@ -6,4 +6,12 @@ def t_recursive_sum (x, sum):
     else:
         return t_recursive_sum(x-1, x+sum)
 
-print(t_recursive_sum(25, 0))
+print(t_recursive_sum(997, 0))
+
+def recursive_sum(x):
+    if x == 1:
+        return x
+    else:
+        return x + recursive_sum(x - 1)
+
+print(recursive_sum(997))
