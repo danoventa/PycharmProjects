4f1d2e52b0c139bf8e64da8ee227622075b3b877
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-06-08 22:56:44 -0500

    Completed simple average and std of poll surveys.

--- DataAnalysis/sakila-db/ElectionDataAnalysis.py
+++ DataAnalysis/sakila-db/ElectionDataAnalysis.py
@@ -1,6 +1,8 @@
 from __future__ import division
 __author__ = 'Noventa'
 
+import matplotlib
+matplotlib.use('TkAgg')
 
 import pandas as pd
 from pandas import Series, DataFrame
@@ -11,7 +13,30 @@ sns.set_style('whitegrid')
 
 import requests
 
+''' no need to do strinio since we're not working with python 2.7 '''
+
 url = 'http://elections.huffingtonpost.com/pollster/2012-general-election-romney-vs-obama.csv'
 source = pd.read_csv(url)
 
 print(source.info())
+print(source.head())
+
+sns.factorplot('Affiliation', data=source)
+plt.show()
+
+sns.factorplot('Affiliation', data=source, hue='Population')
+plt.show()
+
+avg = pd.DataFrame(source.mean())
+avg.drop('Number of Observations', axis=0, inplace=True)
+
+std = pd.DataFrame(source.std())
+std.drop('Number of Observations', axis=0, inplace=True)
+std.head()
+
+avg.plot(yerr=std, kind='bar', legend=False)
+plt.show()
+
+poll_avg = pd.concat([avg, std], axis=1)
+poll_avg.columns = ['Average', 'STD']
+print(poll_avg)
