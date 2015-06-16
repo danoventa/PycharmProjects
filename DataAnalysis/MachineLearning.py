d0e2020b930e296674ffa9d314f754a87c4974ac
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-06-15 23:25:29 -0500

    Looking at one of the practice datasets.

--- DataAnalysis/MachineLearning.py
+++ DataAnalysis/MachineLearning.py
@@ -3,3 +3,15 @@ __author__ = 'Noventa'
 import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('whitegrid')
+
+from sklearn.datasets import load_boston
+
+boston = load_boston()
+
+print(boston.DESCR)
