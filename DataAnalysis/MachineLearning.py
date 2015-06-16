d31c723cb21114718335be8d54267c85a5b6012c
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-06-15 23:36:37 -0500

    Finished the first lecture with applied machine learning.

--- DataAnalysis/MachineLearning.py
+++ DataAnalysis/MachineLearning.py
@@ -15,3 +15,30 @@ from sklearn.datasets import load_boston
 boston = load_boston()
 
 print(boston.DESCR)
+
+plt.hist(boston.target, bins=50)
+
+plt.xlabel('Prices in $1000s')
+plt.ylabel('Number of Houses')
+
+plt.show()
+
+plt.scatter(boston.data[:, 5], boston.target)
+
+
+plt.ylabel('Price in $1000s')
+plt.xlabel('Number of rooms')
+
+boston_df = DataFrame(boston.data)
+
+boston_df.columns = boston.feature_names
+
+print(boston_df.head())
+
+boston_df['Price'] = boston.target
+
+boston_df.head()
+
+sns.lmplot('RM', 'Price', data=boston_df)
+plt.show()
+
