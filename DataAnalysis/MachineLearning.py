515858d995bb65b7216f0b246677415aa79dfd4a
Daniel A. Noventa <Daniel.A.Noventa@gmail.com>
2015-06-17 00:04:26 -0500

    Completed the regressions series
    Will have to go over teh additional information in th enotebooks.

--- DataAnalysis/MachineLearning.py
+++ DataAnalysis/MachineLearning.py
@@ -62,6 +62,7 @@ plt.plot(x, m*x + b, 'r', label='Best Fit Line')
 
 plt.show()
 
+
 result = np.linalg.lstsq(X, Y)
 
 error_total = result[1]
@@ -80,7 +81,7 @@ Y_target = boston_df.Price
 
 lreg.fit(X_multi, Y_target)
 print('The estimated intercept coefficient is %.2f ' %lreg.intercept_)
-print('the number of coefficients used was %d' %len(lreg.coef))
+print('the number of coefficients used was %d' %len(lreg.coef_))
 
 coeff_df = DataFrame(boston_df.columns)
 coeff_df.columns = ['Features']
@@ -88,3 +89,28 @@ coeff_df.columns = ['Features']
 coeff_df['Coefficient Estimate'] = pd.Series(lreg.coef_)
 print(coeff_df)
 
+X_train, X_test, Y_train, Y_test = sklearn.cross_validation.train_test_split(X, boston_df.Price)
+
+print(X_train.shape, X_test.shape, Y_train.shape, Y_test.shape)
+
+lreg = LinearRegression()
+lreg.fit(X_train, Y_train)
+
+pred_train = lreg.predict(X_train)
+pred_test = lreg.predict(X_test)
+
+print('fit a model Z_train, and calculate the MSE with Y_train: %.2f' %np.mean((Y_train-pred_train)**2))
+print('fits a model X_train, and calculate MSE with X_test and Y_test: %.2f' %np.mean((Y_test-pred_test)**2))
+
+
+train = plt.scatter(pred_train, (pred_train - Y_train), c='b', alpha=0.5)
+
+test = plt.scatter(pred_test, (pred_test-Y_test), c='r', alpha=0.5)
+
+plt.hlines(y=0, xmin=-10, xmax=10)
+
+plt.legent((train, test), ('Training', 'Test'), loc='lower left')
+
+plt.title('Residual Plots')
+
+import statsmodels.api as sm
