d6aaadbf91d87d57f0566e87f44fe26d566d8be7
Daniel A. Noventa <Daniel.A.Noventa@gmail.com>
2015-06-18 23:01:46 -0500

    Affairs data discovery
    Interesting... education, and occupation might be good to compare. Also,
    appears that the rating is very closely related to the likelyhood of
    being in an affair.

--- DataAnalysis/MachineLearning2.py
+++ DataAnalysis/MachineLearning2.py
@@ -27,4 +27,41 @@ plt.plot(t, y)
 plt.title(' Logistic Function ')
 plt.show()
 
+df = sm.datasets.fair.load_pandas().data
+
+print(df.head())
+
+def affair_check(x):
+    if x != 0:
+        return 1
+    else:
+        return 0
+
+df['Had_Affair'] = df['affairs'].apply(affair_check)
+
+print(df.head())
+
+df.groupby('Had_Affair').mean()
+
+sns.factorplot('age', data=df, hue='Had_Affair', palette='coolwarm')
+
+
+sns.factorplot('yrs_married', data=df, hue='Had_Affair', palette='coolwarm')
+
+sns.factorplot('children', data=df, hue='Had_Affair', palette='coolwarm')
+
+sns.factorplot('educ', data=df, hue='Had_Affair', palette='coolwarm')
+
+sns.factorplot('religious', data=df, hue='Had_Affair', palette='coolwarm')
+
+sns.factorplot('rate_marriage', data=df, hue='Had_Affair', palette='coolwarm')
+
+sns.factorplot('occupation', data=df, hue='Had_Affair', palette='coolwarm')
+
+
+sns.factorplot('affairs', data=df, hue='Had_Affair', palette='coolwarm')
+
+
+sns.factorplot('occupation_husb', data=df, hue='Had_Affair', palette='coolwarm')
+plt.show()
 
