9b90869b38a77e2e1d7591d8603196b5e6e0fc49
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-06-27 21:10:17 -0500

    an error with the index. Index is out of bounds.

--- DataAnalysis/MachineLearning3.py
+++ DataAnalysis/MachineLearning3.py
@@ -1,7 +1,85 @@
 __author__ = 'Noventa'
 
 
-import numpy as py
+import numpy as np
 import pandas as pd
 import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+X = iris.data
+Y = iris.target
+
+print(iris.DESCR)
+
+from sklearn.svm import SVC
+# the support vector c
+
+model = SVC()
+
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+model.fit(X_train, Y_train)
+
+from sklearn import metrics
+
+predicted = model.predict(X_test)
+expected = Y_test
+
+print(metrics.accuracy_score(expected, predicted))
+
+from sklearn import svm
+
+X = iris.data[:,:2]
+Y = iris.target
+
+C = 1.0
+
+svc = svm.SVC(kernel='linear', C=C).fit(X, Y)
+
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, Y)
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, Y)
+lin_svc = svm.LinearSVC(C=C).fit(X, Y)
+
+h = 0.02
+
+x_min = X[:,0].min() - 1
+x_max = X[:,0].max() + 1
+
+y_min = X[:,1].min() - 1
+y_max = X[:,1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+titles = ['SVC with linear kernel',
+          'LinearSVC (linear kernel'
+          'SVC with RBF kernel',
+          'SVC with polynomial (degree 3) kernel']
+
+for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
+    plt.figure(figsize=(15, 15))
+    plt.subplot(2, 2, i+1)
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    Z = Z.reshape(xx.shape)
+
+    plt.contourf(xx, yy, Z, cmap=plt.cm.terrain, alpja=0.5, linewidths = 0)
+    plt.scatter(X[:,0], X[:, 1], c=Y, cmap=plt.cm.Dark2)
+
+    plt.xlabel('Sepal length')
+    plt.ylabel('Sepal width')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+    plt.xticks(())
+    plt.yticks(())
+    plt.title(titles[i])
+
+plt.show()
 
